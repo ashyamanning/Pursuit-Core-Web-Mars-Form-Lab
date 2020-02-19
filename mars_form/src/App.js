bf90f2d2555b84133f074127e0b3619e4cd0153b
ashya <ashyamanning@pursuit.org>
2020-02-19 18:55:04 -0500

    create app and import Form.js into index.js

---
+++ mars_form/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hello beautiful daughter of God!
+    </div>
+  );
+}
+
+export default App;
